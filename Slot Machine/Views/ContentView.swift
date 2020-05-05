15c3b4bd731aa13c9608b565182acbb882dbb62f
Artur Shamsutdinov <rocketstormnet@outlook.com>
2020-05-05 14:29:14 +0500

    Init and setup

---
+++ Slot Machine/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Machine
+//
+//  Created by Artur Shamsutdinov on 05.05.2020.
+//  Copyright © 2020 Artur Shamsutdinov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
